1be8e4eac02ddc84811640679085e97589ba9f06
Artit <lekiseinno001@gmail.com>
2019-01-26 17:04:27 +0700

    Footer Setup

--- LFB_CREATE_ITEM/setup_master.Designer.vb
+++ LFB_CREATE_ITEM/setup_master.Designer.vb
@@ -22,32 +22,42 @@ Partial Class setup_master
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(setup_master))
-        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.header = New System.Windows.Forms.MenuStrip()
         Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ConnectDataBaseToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.ConnectionToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
         Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
-        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.footer = New System.Windows.Forms.StatusStrip()
+        Me.lb_datenow = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripStatusLabel5 = New System.Windows.Forms.ToolStripStatusLabel()
         Me.lb_IPAddress = New System.Windows.Forms.ToolStripStatusLabel()
-        Me.lb_AnyDesk = New System.Windows.Forms.ToolStripStatusLabel()
-        Me.lb_TeamView = New System.Windows.Forms.ToolStripStatusLabel()
         Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.lb_AnyDesk = New System.Windows.Forms.ToolStripStatusLabel()
         Me.ToolStripStatusLabel2 = New System.Windows.Forms.ToolStripStatusLabel()
-        Me.ConnectDataBaseToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.MenuStrip1.SuspendLayout()
-        Me.StatusStrip1.SuspendLayout()
+        Me.lb_TeamView = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripStatusLabel3 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripProgressBar1 = New System.Windows.Forms.ToolStripProgressBar()
+        Me.timer_datenow = New System.Windows.Forms.Timer(Me.components)
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.ToolStripStatusLabel4 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.lb_computerName = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.header.SuspendLayout()
+        Me.footer.SuspendLayout()
         Me.SuspendLayout()
         '
-        'MenuStrip1
+        'header
         '
-        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
-        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
-        Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(1008, 24)
-        Me.MenuStrip1.TabIndex = 0
-        Me.MenuStrip1.Text = "MenuStrip1"
+        Me.header.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.header.Location = New System.Drawing.Point(0, 0)
+        Me.header.Name = "header"
+        Me.header.Size = New System.Drawing.Size(1350, 24)
+        Me.header.TabIndex = 0
+        Me.header.Text = "MenuStrip1"
         '
         'FileToolStripMenuItem
         '
@@ -56,36 +66,54 @@ Partial Class setup_master
         Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
         Me.FileToolStripMenuItem.Text = "File"
         '
+        'ConnectDataBaseToolStripMenuItem
+        '
+        Me.ConnectDataBaseToolStripMenuItem.Name = "ConnectDataBaseToolStripMenuItem"
+        Me.ConnectDataBaseToolStripMenuItem.Size = New System.Drawing.Size(172, 22)
+        Me.ConnectDataBaseToolStripMenuItem.Text = "Connect DataBase"
+        '
         'ConnectionToolStripMenuItem
         '
         Me.ConnectionToolStripMenuItem.Name = "ConnectionToolStripMenuItem"
-        Me.ConnectionToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ConnectionToolStripMenuItem.Size = New System.Drawing.Size(172, 22)
         Me.ConnectionToolStripMenuItem.Text = "Cechk Connection"
         '
         'ToolStripSeparator1
         '
         Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
-        Me.ToolStripSeparator1.Size = New System.Drawing.Size(177, 6)
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(169, 6)
         '
         'ExitToolStripMenuItem
         '
         Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
-        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(172, 22)
         Me.ExitToolStripMenuItem.Text = "Exit"
         '
         'ToolStripSeparator2
         '
         Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
-        Me.ToolStripSeparator2.Size = New System.Drawing.Size(177, 6)
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(169, 6)
         '
-        'StatusStrip1
+        'footer
         '
-        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.lb_IPAddress, Me.ToolStripStatusLabel1, Me.lb_AnyDesk, Me.ToolStripStatusLabel2, Me.lb_TeamView})
-        Me.StatusStrip1.Location = New System.Drawing.Point(0, 461)
-        Me.StatusStrip1.Name = "StatusStrip1"
-        Me.StatusStrip1.Size = New System.Drawing.Size(1008, 22)
-        Me.StatusStrip1.TabIndex = 1
-        Me.StatusStrip1.Text = "StatusStrip1"
+        Me.footer.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.lb_datenow, Me.ToolStripStatusLabel4, Me.lb_computerName, Me.ToolStripStatusLabel5, Me.lb_IPAddress, Me.ToolStripStatusLabel1, Me.lb_AnyDesk, Me.ToolStripStatusLabel2, Me.lb_TeamView, Me.ToolStripStatusLabel3, Me.ToolStripProgressBar1})
+        Me.footer.Location = New System.Drawing.Point(0, 707)
+        Me.footer.Name = "footer"
+        Me.footer.Size = New System.Drawing.Size(1350, 22)
+        Me.footer.TabIndex = 1
+        Me.footer.Text = "StatusStrip1"
+        '
+        'lb_datenow
+        '
+        Me.lb_datenow.Name = "lb_datenow"
+        Me.lb_datenow.Size = New System.Drawing.Size(68, 17)
+        Me.lb_datenow.Text = "lb_datenow"
+        '
+        'ToolStripStatusLabel5
+        '
+        Me.ToolStripStatusLabel5.Name = "ToolStripStatusLabel5"
+        Me.ToolStripStatusLabel5.Size = New System.Drawing.Size(16, 17)
+        Me.ToolStripStatusLabel5.Text = " | "
         '
         'lb_IPAddress
         '
@@ -93,11 +121,23 @@ Partial Class setup_master
         Me.lb_IPAddress.Size = New System.Drawing.Size(73, 17)
         Me.lb_IPAddress.Text = "lb_IPAddress"
         '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(16, 17)
+        Me.ToolStripStatusLabel1.Text = " | "
+        '
         'lb_AnyDesk
         '
         Me.lb_AnyDesk.Name = "lb_AnyDesk"
         Me.lb_AnyDesk.Size = New System.Drawing.Size(68, 17)
         Me.lb_AnyDesk.Text = "lb_AnyDesk"
+        '
+        'ToolStripStatusLabel2
+        '
+        Me.ToolStripStatusLabel2.Name = "ToolStripStatusLabel2"
+        Me.ToolStripStatusLabel2.Size = New System.Drawing.Size(16, 17)
+        Me.ToolStripStatusLabel2.Text = " | "
         '
         'lb_TeamView
         '
@@ -105,56 +145,98 @@ Partial Class setup_master
         Me.lb_TeamView.Size = New System.Drawing.Size(76, 17)
         Me.lb_TeamView.Text = "lb_TeamView"
         '
-        'ToolStripStatusLabel1
+        'ToolStripStatusLabel3
         '
-        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
-        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(10, 17)
-        Me.ToolStripStatusLabel1.Text = "|"
+        Me.ToolStripStatusLabel3.Name = "ToolStripStatusLabel3"
+        Me.ToolStripStatusLabel3.Size = New System.Drawing.Size(16, 17)
+        Me.ToolStripStatusLabel3.Text = " | "
         '
-        'ToolStripStatusLabel2
+        'ToolStripProgressBar1
         '
-        Me.ToolStripStatusLabel2.Name = "ToolStripStatusLabel2"
-        Me.ToolStripStatusLabel2.Size = New System.Drawing.Size(10, 17)
-        Me.ToolStripStatusLabel2.Text = "|"
+        Me.ToolStripProgressBar1.Name = "ToolStripProgressBar1"
+        Me.ToolStripProgressBar1.Size = New System.Drawing.Size(100, 16)
         '
-        'ConnectDataBaseToolStripMenuItem
+        'timer_datenow
         '
-        Me.ConnectDataBaseToolStripMenuItem.Name = "ConnectDataBaseToolStripMenuItem"
-        Me.ConnectDataBaseToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
-        Me.ConnectDataBaseToolStripMenuItem.Text = "Connect DataBase"
+        Me.timer_datenow.Enabled = True
+        Me.timer_datenow.Interval = 1000
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Location = New System.Drawing.Point(8, 27)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 673)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Location = New System.Drawing.Point(214, 27)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(1128, 673)
+        Me.GroupBox2.TabIndex = 3
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "GroupBox2"
+        '
+        'ToolStripStatusLabel4
+        '
+        Me.ToolStripStatusLabel4.Name = "ToolStripStatusLabel4"
+        Me.ToolStripStatusLabel4.Size = New System.Drawing.Size(16, 17)
+        Me.ToolStripStatusLabel4.Text = " | "
+        '
+        'lb_computerName
+        '
+        Me.lb_computerName.Name = "lb_computerName"
+        Me.lb_computerName.Size = New System.Drawing.Size(106, 17)
+        Me.lb_computerName.Text = "lb_computerName"
         '
         'setup_master
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1008, 483)
-        Me.Controls.Add(Me.StatusStrip1)
-        Me.Controls.Add(Me.MenuStrip1)
+        Me.ClientSize = New System.Drawing.Size(1350, 729)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.footer)
+        Me.Controls.Add(Me.header)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
-        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MainMenuStrip = Me.header
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(1366, 768)
+        Me.MinimumSize = New System.Drawing.Size(1366, 768)
         Me.Name = "setup_master"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Config - Master"
-        Me.MenuStrip1.ResumeLayout(False)
-        Me.MenuStrip1.PerformLayout()
-        Me.StatusStrip1.ResumeLayout(False)
-        Me.StatusStrip1.PerformLayout()
+        Me.header.ResumeLayout(False)
+        Me.header.PerformLayout()
+        Me.footer.ResumeLayout(False)
+        Me.footer.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents header As MenuStrip
     Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
     Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents ConnectionToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents ToolStripSeparator2 As ToolStripSeparator
-    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents footer As StatusStrip
     Friend WithEvents lb_IPAddress As ToolStripStatusLabel
     Friend WithEvents lb_AnyDesk As ToolStripStatusLabel
     Friend WithEvents lb_TeamView As ToolStripStatusLabel
     Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
     Friend WithEvents ToolStripStatusLabel2 As ToolStripStatusLabel
     Friend WithEvents ConnectDataBaseToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripStatusLabel3 As ToolStripStatusLabel
+    Friend WithEvents ToolStripProgressBar1 As ToolStripProgressBar
+    Friend WithEvents lb_datenow As ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLabel5 As ToolStripStatusLabel
+    Friend WithEvents timer_datenow As Timer
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents ToolStripStatusLabel4 As ToolStripStatusLabel
+    Friend WithEvents lb_computerName As ToolStripStatusLabel
 End Class
--- LFB_CREATE_ITEM/setup_master.vb
+++ LFB_CREATE_ITEM/setup_master.vb
@@ -8,7 +8,68 @@
     End Sub
 
     Private Sub setup_master_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'lb_connection_file.Text = fileserver
+        set_footers()
+        cmds()
     End Sub
 
+    Private Sub timer_datenow_Tick(sender As Object, e As EventArgs) Handles timer_datenow.Tick
+        lb_datenow.Text = Now.ToString("dd-MM-yyyy HH:mm:ss")
+    End Sub
+
+
+    Sub set_footers()
+        lb_computerName.Text = "Computername : " & Net.Dns.GetHostName()
+        'array ipaddress
+        lb_IPAddress.Text = "IPV4 : " & Net.Dns.GetHostByName(Net.Dns.GetHostName()).AddressList(3).ToString()
+    End Sub
+
+
+    Sub get_IP()
+
+        Dim strHostName As String
+
+        Dim strIPAddress As String
+
+
+
+        strHostName = Net.Dns.GetHostName()
+
+        strIPAddress = Net.Dns.GetHostByName(strHostName).AddressList(3).ToString()
+
+
+        MessageBox.Show("Host Name: " & strHostName & "; IP Address: " & strIPAddress)
+
+    End Sub
+
+
+
+
+    Sub cmds()
+        Dim CMDprocess As New Process
+        Dim StartInfo As New System.Diagnostics.ProcessStartInfo
+        StartInfo.FileName = "cmd"
+        StartInfo.CreateNoWindow = True
+        StartInfo.RedirectStandardInput = True
+        StartInfo.RedirectStandardOutput = True
+        StartInfo.UseShellExecute = False
+        CMDprocess.StartInfo = StartInfo
+        CMDprocess.Start()
+
+        Dim SR As System.IO.StreamReader = CMDprocess.StandardOutput
+        Dim SW As System.IO.StreamWriter = CMDprocess.StandardInput
+
+        'SW.WriteLine("ping 8.8.8.8")
+
+        'SW.WriteLine("exit")
+
+        MsgBox(SR.ReadToEnd)
+
+        SW.Close()
+        SR.Close()
+
+
+        ' Process.Start("cmd", "/c reg query HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\TeamViewer /v ClientID")
+        ' Dim id = Process.Start("cmd", "/c reg query HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\TeamViewer /v ClientID").ToString()
+        ' MsgBox(output)
+    End Sub
 End Class
