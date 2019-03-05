684530739c2cba578c0070ae099fb8e6e96cb1b5
Artit <lekiseinno001@gmail.com>
2019-03-05 10:15:01 +0700

    05032019

--- LFB_CREATE_ITEM/data_PL.vb
+++ LFB_CREATE_ITEM/data_PL.vb
@@ -30,4 +30,38 @@ Public Class data_PL
             r.Height = 500
         Next
     End Sub
+
+    Private Sub txt_search_TextChanged(sender As Object, e As EventArgs) Handles txt_search.TextChanged
+        Try
+
+            Dim sql As String
+            sql = "SELECT * FROM [LFB_ITEM$].[dbo].[LFB_ITEM$_PL]"
+            sql &= "WHERE a Like '%" & txt_search.Text & "%' "
+            sql &= "OR b Like '%" & txt_search.Text & "%' "
+            sql &= "OR c Like '%" & txt_search.Text & "%' "
+            sql &= "OR d Like '%" & txt_search.Text & "%' "
+            sql &= "OR e Like '%" & txt_search.Text & "%' "
+            sql &= "OR f Like '%" & txt_search.Text & "%' "
+            sql &= "OR g Like '%" & txt_search.Text & "%' "
+            sql &= "OR h Like '%" & txt_search.Text & "%' "
+            sql &= "OR i Like '%" & txt_search.Text & "%' "
+            Dim query As New SqlCommand(sql, connection)
+            Dim dataadapter As New SqlDataAdapter(query)
+            Dim ds As New DataSet()
+            dataadapter.Fill(ds, "a")
+            DataGrid_PL.DataSource = ds
+            DataGrid_PL.DataMember = "a"
+
+
+
+
+
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical)
+        End Try
+    End Sub
+
+    Private Sub DataGrid_PL_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGrid_PL.CellClick
+        'MsgBox()
+    End Sub
 End Class
